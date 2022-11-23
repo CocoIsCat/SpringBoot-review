604a9957d814765d112e426cd26a1a312e2af39a
cocoiscat <demy176@naver.com>
2022-11-23 15:02:54 +0900

    feat: DTO 객체 구현

---
+++ day1/todoCalendar/src/main/java/com/example/todocalendar/dto/DTO.java
@@ -0,0 +1,22 @@
+package com.example.todocalendar.dto;
+
+public class DTO {
+    private String date;
+    private String title;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
