d472817302a5dda7b90e341735060b48939439e0
cocoiscat <demy176@naver.com>
2022-11-23 15:42:25 +0900

    feat: 캘린더의 전체 목록을 반환하는 기능 구현
    "all/schedule"로 요청이 들어왔을 때 할 일 목록을 반환하는 컨트롤러를 구현함

---
+++ day1/todoCalendar/src/main/java/com/example/todocalendar/calendarController/CalendarController.java
@@ -0,0 +1,18 @@
+package com.example.todocalendar.calendarController;
+
+import com.example.todocalendar.dto.Calendar;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class CalendarController {
+
+    ArrayList<Calendar> todoCalendar = new ArrayList<>();
+
+    @GetMapping("all/schedule")
+    public ArrayList<Calendar> allSchedule() {
+        return todoCalendar;
+    }
+}
