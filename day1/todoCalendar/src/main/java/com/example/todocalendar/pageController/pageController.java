4bd5680a7e9b5d8a940fbf8da368776748725805
cocoiscat <demy176@naver.com>
2022-11-23 15:33:18 +0900

    feat: 초기 페이지 이동 설정
    진입했을 때 index 페이지로 넘어가도록 설정함

--- day1/todoCalendar/src/main/java/com/example/todocalendar/pageController/pageController.java
+++ day1/todoCalendar/src/main/java/com/example/todocalendar/pageController/pageController.java
@@ -1,2 +1,13 @@
-package com.example.todocalendar.pageController;public class pageController {
+package com.example.todocalendar.pageController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class pageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
