f4085e85d8478df6c4074632feb1254c7410c12e
cocoiscat <demy176@naver.com>
2022-11-24 18:54:48 +0900

    feat: 로그인 서비스 구현

--- day2/login-logout/src/main/java/com/example/loginlogout/service/login/Login.java
+++ day2/login-logout/src/main/java/com/example/loginlogout/service/login/Login.java
@@ -1,2 +1,25 @@
-package com.example.loginlogout.service.login;public class Login {
+package com.example.loginlogout.service.login;
+
+import com.example.loginlogout.dto.Member;
+import com.example.loginlogout.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Login {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public Login(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public boolean login(Member member) {
+        if (memberRepository.findId(member.getEmail()).get().getPassword().equals(member.getPassword())) {
+            return true;
+        }
+        else
+            return false;
+    }
 }
