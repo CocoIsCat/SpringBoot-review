13292c30cd71e3840e67aecc91ce96b8f4cd2039
cocoiscat <demy176@naver.com>
2022-11-24 19:08:37 +0900

    feat: 회원가입 컨트롤러 구현

---
+++ day2/login-logout/src/main/java/com/example/loginlogout/controller/signup/controller/SignUpController.java
@@ -0,0 +1,26 @@
+package com.example.loginlogout.controller.signup.controller;
+
+import com.example.loginlogout.dto.Member;
+import com.example.loginlogout.service.signup.SignUp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SignUpController {
+
+
+    private final SignUp signUp;
+
+    @Autowired
+    public SignUpController(SignUp signUp) {
+        this.signUp = signUp;
+    }
+
+    @PostMapping("/signup")
+    public String signup(Member member) {
+        signUp.join(member);
+        System.out.println("회원가입 성공!");
+        return "index";
+    }
+}
