4847107577be76dcfbfa7fd302cd83e6e69fd6f7
cocoiscat <demy176@naver.com>
2022-11-25 19:48:59 +0900

    faet: 회원 전용 페이지 이동 구현
    로그인이 되어있으면 index 페이지로,로그인 되어있지 않으면 login페이지로 이동

--- day2/login-logout/src/main/java/com/example/loginlogout/controller/page/controller/PageController.java
+++ day2/login-logout/src/main/java/com/example/loginlogout/controller/page/controller/PageController.java
@@ -1,10 +1,22 @@
-package com.example.loginlogout.page.controller;
+package com.example.loginlogout.controller.page.controller;
 
+import com.example.loginlogout.service.login.Login;
+import org.apache.juli.logging.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class PageController {
+
+    private final Login login;
+
+    @Autowired
+    public PageController(Login login) {
+        this.login = login;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -19,5 +31,14 @@ public class PageController {
     @GetMapping("/goLogin")
     public String goLogin() {
         return "login";
+    }
+
+    @GetMapping("/members")
+    public String goMembers(HttpSession session) {
+        if(login.checkLogin(session)) {
+            return "index";
+        }
+        else
+            return "login";
     }
 }
