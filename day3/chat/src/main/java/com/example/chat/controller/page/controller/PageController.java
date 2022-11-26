02c6ba396196590c4d43d52bd0e7859192b8842b
cocoiscat <demy176@naver.com>
2022-11-26 21:53:05 +0900

    feat: main 페이지로 넘어가는 부분 구현
    로그인에 성공하면 main 페이지로 넘어감

--- day3/chat/src/main/java/com/example/chat/controller/page/controller/PageController.java
+++ day3/chat/src/main/java/com/example/chat/controller/page/controller/PageController.java
@@ -1,15 +1,33 @@
 package com.example.chat.controller.page.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.example.chat.service.LoginService;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class PageController {
 
+    private final LoginService loginservice;
+
+    @Autowired
+    public PageController(LoginService loginservice) {
+        this.loginservice = loginservice;
+    }
+
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/goMain")
+    public String goMain(String nickName, HttpSession session) {
+        if (loginservice.login(nickName, session)) {
+            return "main";
+        }
+        else
+            return "index";
     }
 }
