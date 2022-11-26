374b32b0a008a4c4fb4e5d906021acbe74d0e5e8
cocoiscat <demy176@naver.com>
2022-11-26 21:55:47 +0900

    feat: 채팅 기록 저장소 생성

---
+++ day3/chat/src/main/java/com/example/chat/repository/ChatRepository.java
@@ -0,0 +1,27 @@
+package com.example.chat.repository;
+
+import org.springframework.stereotype.Repository;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+@Repository
+public class ChatRepository {
+    ArrayList<ArrayList<String>> storage = new ArrayList<>();
+
+    public void save(String ch, HttpSession session) {
+        ArrayList<String> chat = new ArrayList<>();
+
+        String nickName=String.valueOf(session.getAttribute("nickName"));
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        chat.add(String.valueOf(session.getAttribute("nickName")));
+        chat.add(ch);
+        chat.add(dtf.format(now));
+
+        storage.add(chat);
+    }
+}
