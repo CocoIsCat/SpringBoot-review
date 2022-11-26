3078815b6f3677cbc5c7794ff113063d8dde7d16
cocoiscat <demy176@naver.com>
2022-11-26 21:26:15 +0900

    feat: 홈으로 index페이지 지정

---
+++ day3/chat/src/main/java/com/example/chat/controller/page/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.example.chat.controller.page.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
