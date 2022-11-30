401d2e24a7309992ccd0a5b6fcd17622cd3a5aec
cocoiscat <demy176@naver.com>
2022-11-30 20:23:46 +0900

    Initial Commit

---
+++ day5/registerMybatis/src/test/java/com/example/registermybatis/RegisterMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registermybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
