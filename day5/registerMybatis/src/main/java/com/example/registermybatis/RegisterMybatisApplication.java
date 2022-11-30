0d7bb4d2b9b51543f34f813ed7c3c03b4a7d4413
cocoiscat <demy176@naver.com>
2022-11-30 20:33:32 +0900

    Initial Commit

---
+++ day5/registerMybatis/src/main/java/com/example/registermybatis/RegisterMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.registermybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterMybatisApplication.class, args);
+    }
+
+}
